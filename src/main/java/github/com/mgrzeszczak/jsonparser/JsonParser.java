4450c469662d177b07824920e97a8e9bd35c0d4e
Maciej Grzeszczak <maciejgrzeszczak@gmail.com>
2017-06-15 10:38:10 +0200

    Make JsonParser's constructor private.

--- src/main/java/github/com/mgrzeszczak/jsonparser/JsonParser.java
+++ src/main/java/github/com/mgrzeszczak/jsonparser/JsonParser.java
@@ -4,6 +4,10 @@ import github.com.mgrzeszczak.jsonparser.json.JsonNode;
 
 public class JsonParser {
 
+    private JsonParser() {
+        throw new AssertionError("no instances");
+    }
+
     public static JsonNode parse(String json) {
         return Parser.of(Tokenizer.of(json)).parse();
     }
